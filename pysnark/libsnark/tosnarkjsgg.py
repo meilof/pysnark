8059f58881b8fd308448a7984e2f6d7cc79aea45
Meilof Veeningen <meilof@gmail.com>
2020-11-02 21:09:40 +0100

    update tosnarkjsgg to latest snarkjs

--- pysnark/libsnark/tosnarkjsgg.py
+++ pysnark/libsnark/tosnarkjsgg.py
@@ -2,22 +2,24 @@ vkin = open("pysnark_vk", "r")
 vkout = open("verification_key.json", "w")
 
 print('{', file=vkout)
-print(' "protocol": "groth",', file=vkout)
+print(' "protocol": "groth16",', file=vkout)
+print(' "curve": "bn128",', file=vkout)
 
 def process_g2(nm, fin, fout, skipcomma=False):
     a1=list(map(lambda x:x.strip(),next(fin).split(" ")))
     a2=list(map(lambda x:x.strip(),next(fin).split(" ")))
+    a3=list(map(lambda x:x.strip(),next(fin).split(" ")))
     print(' "' + nm + '": [', file=fout)
     print('   [ "' + a1[0] + '", "' + a1[1] + '"],', file=fout)
     print('   [ "' + a2[0] + '", "' + a2[1] + '"],', file=fout)
-    print('   [ "1", "0" ] ]' + ('' if skipcomma else ','), file=fout)
+    print('   [ "' + a3[0] + '", "' + a3[1] + '"] ]' + ('' if skipcomma else ','), file=fout)
 
 def process_g1(nm, fin, fout):
-    b=[next(fin).strip(), next(fin).strip()]
-    print(' "' + nm + '": ["' + b[0] + '", "' + b[1] + '", "1" ],', file=fout)
+    b=[next(fin).strip(), next(fin).strip(), next(fin).strip()]
+    print(' "' + nm + '": ["' + b[0] + '", "' + b[1] + '", "' + b[2] + '" ],', file=fout)
     
 f12=list(map(lambda x:x.strip(),next(vkin).split(" ")))
-print(' "vk_alfabeta_12": [', file=vkout)
+print(' "vk_alphabeta_12": [', file=vkout)
 print('   [', file=vkout)
 print('    ["' + f12[0] + '", "' + f12[1] + '"],', file=vkout)
 print('    ["' + f12[2] + '", "' + f12[3] + '"],', file=vkout)
@@ -37,11 +39,11 @@ print(' "nPublic": ' + str(nin-1) + ',', file=vkout)
 
 print(' "IC": [', file=vkout)
 for i in range(nin):
-    b=[next(vkin).strip(), next(vkin).strip()]
-    print('  ["' + b[0] + '", "' + b[1] + '", "1"]' + (',' if i!=nin-1 else ''), file=vkout)
+    b=[next(vkin).strip(), next(vkin).strip(), next(vkin).strip()]
+    print('  ["' + b[0] + '", "' + b[1] + '", "' + b[2] + '"]' + (',' if i!=nin-1 else ''), file=vkout)
 print(' ],', file=vkout)
 
-process_g1("vk_alfa_1", vkin, vkout)
+process_g1("vk_alpha_1", vkin, vkout)
 process_g2("vk_beta_2", vkin, vkout, True)
 
 print('}', file=vkout)
